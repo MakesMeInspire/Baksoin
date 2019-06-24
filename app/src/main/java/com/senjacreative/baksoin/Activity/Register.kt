49efd58de7e2a9c2f5f7c612f6fec9cb7961a16b
MakesMeInspire <muhammad.iqbal.najih@gmail.com>
2019-06-24 15:18:44 +0700

    Update v0.0.1 - Register layout update

--- app/src/main/java/com/senjacreative/baksoin/Activity/Register.kt
+++ app/src/main/java/com/senjacreative/baksoin/Activity/Register.kt
@@ -1,12 +1,18 @@
 package com.senjacreative.baksoin.Activity
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.content.ContextCompat
+import android.text.Html
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
 import android.view.Window
 import android.view.WindowManager
+import android.widget.TextView
 import com.senjacreative.baksoin.R
 import kotlinx.android.synthetic.main.activity_register.*
 
